0613954dec070be29d36032b3457a5724802ccce
unknown <hankggg2003xd@gmail.com>
2022-01-25 13:51:28 +0800

    add gif responses

--- Main.py
+++ Main.py
@@ -3,38 +3,47 @@ import discord
 from discord.ext import commands
 
 with open(".\config.json","r",encoding="UTF8") as config:
-    configs=json.load(config)
+    conf=json.load(config)
+with open(".\settings","r",encoding="UTF8") as settings:
+
 intents=discord.Intents.all()
 
-bot = commands.Bot(command_prefix='~',intents=intents)
+
+bot = commands.Bot(command_prefix="M ",intents=intents)
 
 
 
 @bot.event
-
 async def on_ready():
     channel = bot.get_channel(934773565084033094)
     await channel.send("hey guys")
 
+@bot.event
+
+async def on_member_join(member):
+    channel = bot.get_channel(934773565084033094)
+    await channel.send(f"you trash, {member}")
+
+@bot.event
+
+async def on_member_remove(member):
+    channel = bot.get_channel(934793872712810536)
+    await channel.send(f"bye, {member}")
+    
 @bot.command()
 async def ping(ctx):
     await ctx.send(f"{round(bot.latency*1000)}(ms)")
 
 @bot.command()
-
 async def dance(ctx):
-    pic=discord.File(configs["bully-maguire-dance"])
+    pic=discord.File(settings["bully-maguire-dance"])
     await ctx.send(file=pic)
-# @bot.event
 
-# async def on_member_join(member):
-#     channel = bot.get_channel(934773565084033094)
-#     await channel.send(f"you trash, {member}")
+@bot.command()
+async def pizza(ctx):
+    pic=discord.File(settings["pizza"])
+    await ctx.send(file=pic)
 
-# @bot.event
 
-# async def on_member_remove(member):
-#     channel = bot.get_channel(934793872712810536)
-#     await channel.send(f"bye, {member}")
 
-bot.run(configs["TOKEN"])
+bot.run(conf["TOKEN"])
