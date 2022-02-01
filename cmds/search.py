3715ebbdabab59a51a6ea571308529e88187cf0e
Vincent <hankggg2003xd@gmail.com>
2022-02-01 14:21:18 +0800

    search

---
+++ cmds/search.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulStoneSoup
+import json
+import discord
+import datetime
+from discord.ext import commands
+from core.classes import Cog_Extension
+
+class Search(Cog_Extension):
+
+    @commands.command()
+    async def search(self,ctx,arg):
+        pass
+
+def setup(bot):
+    bot.add_cog(Search(bot))
