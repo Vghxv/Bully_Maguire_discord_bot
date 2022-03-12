05fa744e2d57411e33211a0c4d63f191a3dce4ec
unknown <hankggg2003xd@gmail.com>
2022-03-12 13:02:45 +0800

    money done

--- cmds/moneyback.py
+++ cmds/moneyback.py
@@ -4,14 +4,13 @@ import discord
 
 class Moneyback(Cog_Extension):
     @commands.command()
-    async def money(self,ctx,debtormoney):
-        arr = debtormoney.split()
+    async def money(self,ctx,a1,a2,a3):
         embed=discord.Embed(title="Creditor", description=f"{ctx.author}", color=0xbc3434)
         embed.set_author(name="I need that money @_@")
-        for i in range(len(arr)/3):
-            embed.add_field(name="debtor", value=f"{arr[3*i]}", inline=True)
-            embed.add_field(name="money", value=f"${arr[3*i+1]}", inline=True)
-            embed.add_field(name="detail", value=f"{arr[3*i+2]}", inline=True)
+        
+        embed.add_field(name="debtor", value=f"{a1}", inline=True)
+        embed.add_field(name="money", value=f"${a2}", inline=True)
+        embed.add_field(name="detail", value=f"{a3}", inline=True)
         await ctx.send(embed=embed)
 
     
